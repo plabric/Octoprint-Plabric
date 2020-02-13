461d1b4a74b3e58cf8dd2cf3a1ca7f36340e95ae
angel <it@plabric.com>
2020-02-13 10:38:48 -0800

    [controllers][download] Use queues for update the ui while downloading the docker image

--- octoprint_plabric/controllers/download.py
+++ octoprint_plabric/controllers/download.py
@@ -4,13 +4,13 @@ import requests
 
 
 class DownloadThread(threading.Thread):
-	def __init__(self, url, dest, callback, callback_progress, log):
+	def __init__(self, url, dest, queue_main, queue_progress, log):
 		threading.Thread.__init__(self)
 		self.daemon = True
 		self._url = url
 		self._dest = dest
-		self._callback = callback
-		self._callback_progress = callback_progress
+		self._queue_main = queue_main
+		self._queue_progress = queue_progress
 		self._log = log
 
 	def run(self):
@@ -18,7 +18,7 @@ class DownloadThread(threading.Thread):
 			self.download_url(self._url)
 		except Exception as e:
 			self._log(e)
-			self._callback(False)
+			self._queue_main.put(False)
 
 	def download_url(self, url):
 		self._log("[%s] Downloading %s -> %s" % (self.ident, url, self._dest))
@@ -40,13 +40,13 @@ class DownloadThread(threading.Thread):
 						done = int(100 * dl / total_length)
 						if done != last_percent:
 							last_percent = done
-							self._callback_progress(last_percent)
+							self._queue_progress.put_nowait(last_percent)
 		else:
 			self._log('Error downloading Docker image: %d' % r.status_code)
-			self._callback(False)
+			self._queue_main.put_nowait(False)
 
 		# Retrieve HTTP meta-data
 		self._log(r.status_code)
 		self._log(r.headers['content-type'])
 		self._log(r.encoding)
-		self._callback(r.status_code == 200)
+		self._queue_main.put_nowait(r.status_code == 200)
