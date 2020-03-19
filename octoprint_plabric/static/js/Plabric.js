6dd5f7c2fe3222184d62137466b5ff038ec2cd47
angel <it@plabric.com>
2020-03-19 08:54:42 -0700

    [js] Adapted templates to new procedure

--- octoprint_plabric/static/js/Plabric.js
+++ octoprint_plabric/static/js/Plabric.js
@@ -9,18 +9,21 @@ $(function() {
         var self = this;
 
         self.settings = parameters[0];
-        self.configurated = ko.observable(plabric_variables.configurated);
-        self.config_cancelled = ko.observable(plabric_variables.config_cancelled);
-        self.temp_token = ko.observable(plabric_variables.temp_token);
-        self.docker_running = ko.observable(plabric_variables.docker_running);
-        self.docker_available = ko.observable(plabric_variables.docker_available);
-        self.socket_connected = ko.observable(plabric_variables.socket_connected);
-        self.os = ko.observable(plabric_variables.os);
-        self.machine = ko.observable(plabric_variables.machine);
-        self.installing = ko.observable(plabric_variables.installing);
-        self.install_progress = ko.observable(plabric_variables.install_progress);
-        self.progress_width = ko.observable('0px');
+        self.plabric_token = ko.observable(plabric_variables.plabric_token);
+        self.step = ko.observable(plabric_variables.step);
         self.error = ko.observable(plabric_variables.error);
+        self.loading = ko.observable(plabric_variables.loading);
+
+        self.onDataUpdaterPluginMessage = function (plugin, data) {
+            if (plugin !== "Plabric") {
+                return;
+            } else {
+                self.plabric_token(data.plabric_token);
+                self.step(data.step);
+                self.error(data.error);
+                self.loading(data.loading);
+            }
+        };
 
         self.login = function () {
             $.ajax({
@@ -35,56 +38,14 @@ $(function() {
             });
         };
 
-        self.onDataUpdaterPluginMessage = function (plugin, data) {
-            if (plugin !== "Plabric") {
-                return;
-            } else {
-                self.configurated(data.configurated);
-                self.config_cancelled(data.config_cancelled);
-                self.docker_available(data.docker_available);
-                self.docker_running(data.docker_running);
-                self.temp_token(data.temp_token);
-                self.docker_running(data.docker_running);
-                self.socket_connected(data.socket_connected);
-                self.os(data.os);
-                self.machine(data.machine);
-                self.installing(data.installing);
-                self.install_progress(data.install_progress);
-                self.progress_width(self.install_progress().toString() + '%');
-                self.error(data.error);
-
-                if (self.config_cancelled()) {
-                    self.cancel_config();
-                }
-            }
-        };
-
         self.disable = function () {
             $.ajax({
                 type: "GET",
                 url: "/plugin/Plabric/disable",
                 success: function (data) {
-                    self.configurated(false);
-                    self.temp_token(false);
                 },
                 error: function (error) {
                     console.error("Plabric: Unable to retrieve register status");
-                }
-            });
-        };
-
-        self.cancel_config = function () {
-            self.temp_token(null);
-        };
-
-        self.run_docker = function () {
-            $.ajax({
-                type: "GET",
-                url: "/plugin/Plabric/run_docker",
-                success: function (data) {
-                },
-                error: function (error) {
-                    console.error("Plabric: Unable to run docker");
                 }
             });
         };
--- octoprint_plabric/static/js/Plabric_navbar.js
+++ octoprint_plabric/static/js/Plabric_navbar.js
@@ -5,6 +5,7 @@ $(function() {
         self.settings = parameters[0];
         self.status = ko.observable(plabric_status.status);
         self.status_color = ko.observable(plabric_status.status_color);
+        self.step = ko.observable(plabric_status.step);
         setStatusColor();
 
         self.onDataUpdaterPluginMessage = function(plugin, data) {
@@ -12,18 +13,19 @@ $(function() {
                 return;
             }
             else {
+                self.step(data.step);
                 self.status(data.status);
                 setStatusColor();
             }
         };
 
         function setStatusColor() {
-            if(self.status() === 'Disconnected' || self.status() === 'Not running' || self.status() === 'Login needed' || self.status() === 'Need install' || self.status() == 'Stopped'){
-                self.status_color('#f50057');
-            }else if(self.status() === 'Ready'){
+            if(self.step() === 'connected'){
+                self.status_color('#00A69A');
+            }else if(self.step() === 'ready'){
                 self.status_color('#ff8f00');
             }else{
-                self.status_color('#00A69A');
+                self.status_color('#f50057');
             }
         }
     }
