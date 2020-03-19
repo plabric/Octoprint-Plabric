19359d3e0a12eb09e2d71d03cb583d34ff1d99e3
angel <it@plabric.com>
2020-03-19 08:47:15 -0700

    [controllers][octoprint] Added controller for octoprint api and socket

---
+++ octoprint_plabric/controllers/octoprint/api.py
@@ -0,0 +1,93 @@
+from enum import Enum
+from octoprint_plabric.controllers.common.api import API, APIProtocol
+from octoprint_plabric.controllers.common import logger as _logger
+import requests
+
+
+class OctoprintAPIProtocol(APIProtocol):
+	def on_download_first(self, data):
+		raise NotImplementedError
+
+
+class OctoprintAPI(API):
+
+	def __init__(self, domain):
+		super(OctoprintAPI, self).__init__(domain=domain, name='Octoprint API')
+		# super().__init__(domain=domain, name='Octoprint API')
+		self._api_key = None
+
+	def set_api_key(self, api_key):
+		self._api_key = api_key
+
+	def get_headers(self, data=None):
+		h = {'Content-Type': 'application/json'}
+		if self._api_key:
+			h['X-Api-Key'] = self._api_key
+		if data:
+			h['Content-Length'] = str(len(data))
+		return h
+
+	def request_app_token(self, callback):
+		self.post(path='/plugin/appkeys/request', params={'app': 'Plabric'}, callback=callback)
+
+	def request_api_key(self, app_token, callback):
+		self.get(path='/plugin/appkeys/request/%s' % app_token, callback=callback)
+
+	def login(self, octoprint_api_key, callback):
+		self.set_api_key(octoprint_api_key)
+		self.post(path='/api/login', params={'passive': True}, headers=self.get_headers(), callback=callback)
+
+	def call_method(self, data, callback):
+		action = DataAction(raw=data)
+		if action.method == Method.GET:
+			self.get(path=action.path, headers=self.get_headers(), callback=callback)
+
+		elif action.method == Method.POST:
+			if not action.download_first:
+				self.post(path=action.path, params=action.params, headers=self.get_headers(), callback=callback)
+			else:
+				callback.on_download_first(data)
+		elif action.method == Method.PUT:
+			self.put(path=action.path, params=action.params, headers=self.get_headers(), callback=callback)
+		elif action.method == Method.PATCH:
+			self.patch(path=action.path, params=action.params, headers=self.get_headers(), callback=callback)
+		elif action.method == Method.DELETE:
+			self.delete(path=action.path, params=action.params, headers=self.get_headers(), callback=callback)
+
+	def upload_file(self, data, file_path, callback):
+		_logger.log('Octoprint API: Uploading file')
+		action = DataAction(raw=data)
+		file_name = data['params']['file_name']
+		_logger.log('%s Post file on: %s' % (self._name, self._get_url(action.path)))
+
+		files = {"file": ("%s.gcode" % file_name, open(file_path, "rb").read())}
+		payload = {'path': 'plabric/tmp', 'select': 'true', 'print': 'false'}
+		self._execute(requests.post(self._get_url(action.path), data=payload, files=files, headers={'X-Api-Key': self._api_key}), callback)
+
+
+class Method(Enum):
+	GET = 'get'
+	POST = 'post'
+	PUT = 'put'
+	PATCH = 'patch'
+	DELETE = 'delete'
+	DOWNLOAD = 'download'
+
+
+class DataAction:
+
+	def __init__(self, raw):
+		self.raw = raw
+		self.method = None
+		self.path = None
+		self.params = None
+		self.download_first = False
+		self.parse()
+
+	def parse(self):
+		self.method = Method(self.raw['method'])
+		self.path = self.raw['url']
+		if 'params' in self.raw:
+			self.params = self.raw['params']
+		if self.method == Method.POST and self.raw['api'] == 'files':
+			self.download_first = True
---
+++ octoprint_plabric/controllers/octoprint/socket.py
@@ -0,0 +1,70 @@
+import json as _json
+import threading
+
+import websocket
+from octoprint_plabric.controllers.common import logger as _logger
+
+
+class OctoprintSocketProtocol:
+	def on_event(self, event):
+		raise NotImplementedError
+
+	def connected(self):
+		raise NotImplementedError
+
+	def disconnected(self):
+		raise NotImplementedError
+
+
+class OctoprintSocket:
+
+	def __init__(self, domain, callback):
+		_logger.log('Octoprint Socket: Initializing')
+		self._domain = domain
+		self._ws = None
+		self._callback = callback
+		self._thread = None
+
+	def connect(self, username, session):
+		self._connect(username, session)
+
+	def connected(self):
+		return self._ws and self._ws.sock and self._ws.sock.connected
+
+	def disconnect(self):
+		_logger.log('Octoprint Socket: Disconnecting')
+		if self._ws:
+			self._ws.close()
+			self._ws.keep_running = False
+			self._thread = None
+
+	def _connect(self, username, session):
+		if self._thread is None:
+			_logger.log('Octoprint Socket: Connecting')
+
+			def _on_message(ws, message):
+				# _logger.log('Octoprint socket message recieved: %s' % message)
+				self._callback.on_event(event=message)
+
+			def _on_error(ws, error):
+				_logger.log('Octoprint Socket Error: %s' % error)
+
+			def _on_close(ws):
+				_logger.log('Octoprint Socket: Ws Closed')
+				self._callback.disconnected()
+
+			def _on_open(ws):
+				_logger.log('Octoprint Socket: Ws Opened')
+				self._ws.send(_json.dumps({'auth': '%s:%s' % (username, session)}))
+				self._ws.send(_json.dumps({'throttle': 6}))
+				self._callback.connected()
+
+			try:
+				self._ws = websocket.WebSocketApp("ws://localhost:5000/sockjs/websocket", on_message=_on_message, on_error=_on_error, on_close=_on_close, on_open=_on_open)
+
+				self._thread = threading.Thread(target=self._ws.run_forever)
+				self._thread.daemon = True
+				self._thread.start()
+			except Exception as e:
+				_logger.warn(e)
+
