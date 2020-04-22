95ca7fa513b811a44ab67af13cd9190de8af3e55
angel <it@plabric.com>
2020-04-22 09:35:57 -0700

    [config] Use plabric api v2

--- octoprint_plabric/config.py
+++ octoprint_plabric/config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = False
-HOST_PLABRIC_API = 'https://api.plabric.com'
+HOST_PLABRIC_API = 'https://apiv2.plabric.com/'
 PLABRIC_SOCKET_NAMESPACE = "/octoprint/plugin/socket"
 
 FFMPEG_DIR = os.path.join(BASE_DIR, 'bin', 'ffmpeg')
